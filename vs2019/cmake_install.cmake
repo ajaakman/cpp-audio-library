cbe1f5843ccb3beb33ea2f29f90661dc5d6fd0c8
Artur Jaakman <jaakmanartur@gmail.com>
2019-09-28 16:56:45 +0300

    vs Folder Fix.

--- vs2019/cmake_install.cmake
+++ vs2019/cmake_install.cmake
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/dev/Portfolio/audio-app/cpp-audio/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/dev/Portfolio/audio-app/cpp-audio/vs2019/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
